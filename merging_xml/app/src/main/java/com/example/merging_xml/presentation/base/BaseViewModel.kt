fb33656ecc8f72ac2f487950655b8890ec7462fe
stev3j <mygajeong@gmail.com>
2023-03-05 20:22:25 +0900

    ✨ :: Add Bases

---
+++ merging_xml/app/src/main/java/com/example/merging_xml/presentation/base/BaseViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.merging_xml.presentation.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel() {
+
+    protected val isLoading = MutableLiveData<Boolean>()
+    fun getIsLoading(): LiveData<Boolean> = isLoading
+
+    protected val _errorMsg = MutableLiveData<String>()
+    val errorMsg: LiveData<String> get() = _errorMsg
+
+}
